38d58ec1663afffb456ac8f5ca5631083f6b6c37
Guru Prasad <guru.bhumca@gmail.com>
2022-01-13 16:50:57 +0530

    App.js fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Template for MyKitchen app
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Template for my Kitchen app
+          Learn React
         </a>
       </header>
     </div>
