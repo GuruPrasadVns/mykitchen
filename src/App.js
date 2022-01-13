792463b144d53c0cea00edff9658e98991e04a58
Guru Prasad <guru.bhumca@gmail.com>
2022-01-13 16:46:26 +0530

    docker-compose and .travis.yml

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Template for my Kitchen app
         </a>
       </header>
     </div>
